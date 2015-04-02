790de9d1a24b0ad1096a68a875d9f8eaf0eedef5
Oscar Garcia Pacheco <ingenioapp@gmail.com>
2015-04-01 20:57:46 -0600

    cambios nuevos

---
+++ assets/js/constants.js
@@ -0,0 +1,14 @@
+/**
+ * Archivo de constantes para MiTianguis
+ */
+
+var constants = {
+
+	/* Production */
+	//DOMAIN:"http://localhost:1337/"
+
+	/*  Development */
+	DOMAIN:"http://localhost:1337/",
+  LOGIN_SUCCESS:"login successful",
+  LOGIN_FAILED:"login failed"
+};
