26dbf44133b03fddedb942c9a3cb99a1f5bb4d76
oscar monroy <oscar.monroyg@gmail.com>
2015-04-14 17:43:08 -0500

    merge ventacontroller.js y main.html

--- api/controllers/VentaController.js
+++ api/controllers/VentaController.js
@@ -12,41 +12,6 @@ module.exports = {
 	checkout:function(req,res){
 		var orden = req.allParams();
 		console.log(orden);
-
-		var pago = {
-	       "currency":"MXN",
-	       "amount": 10000,
-	       "description":"GAMELAND MITIANGUIS",
-	       "reference_id":"9839-wolf_pack",
-	       "card": orden.token,
-	       "details": {
-	         "email":orden.email
-	       }
-	     }
-
-		unirest.post("https://api.conekta.io/charges")
-		.auth({user: 'key_fK2GfyxqqvW1KJBxmxbqCw'})
-		.headers({	'Accept': 'application/vnd.conekta-v0.3.0+json',
-					'Content-type': 'application/json'})
-		.send(pago)
-		.end(function(response){
-			console.log(response.body)
-		})
-
-
-		res.json(orden)
-	},
-
-	misVentas:function(request, response){
-	    console.log("*************** CONSULTA MIS VENTAS **************");
-	    var data = request.allParams();
-	    
-	    ProductosVenta.find().populate('producto').populate('venta').exec(function(err,productos){
-	         return response.json(productos);
-	    });
-
-  }
-
 		orden.colonia = orden.selColonia.d_asenta;
 		orden.municipio = orden.selColonia.D_mnpio;
 		orden.ciudad = orden.selColonia.d_ciudad;
@@ -164,5 +129,15 @@ module.exports = {
       		})
 
 	    });
-	}
+	},
+		misVentas:function(request, response){
+	    console.log("*************** CONSULTA MIS VENTAS **************");
+	    var data = request.allParams();
+	    
+	    ProductosVenta.find().populate('producto').populate('venta').exec(function(err,productos){
+	         return response.json(productos);
+	    });
+
+  }
+
 };
