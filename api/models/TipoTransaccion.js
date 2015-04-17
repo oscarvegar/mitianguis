be908f3c56c662ebd407c7948e605fc940ad8f24
lentiummmx <lentium.mmx@gmail.com>
2015-04-17 17:22:53 -0500

    Se agrega TipoTransaccion al modelo

---
+++ api/models/TipoTransaccion.js
@@ -0,0 +1,17 @@
+/**
+* TipoTransaccion.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    ordinal : { type: 'integer' },
+    descripcion : { type: 'string' },
+    codigo : { type: 'string' }
+  }
+
+};
+
